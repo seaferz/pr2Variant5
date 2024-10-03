09687b0b516534f9816c11617c2459893b485fb7
seaferz <86144706+seaferz@users.noreply.github.com>
2024-10-03 14:39:21 +0300

    Update Program.cs

--- pr2Variant5/Program.cs
+++ pr2Variant5/Program.cs
@@ -1,9 +1,11 @@
 ﻿// Дано 4567 копеек определить сколько сотен, рублей и копеек
 int K = 4567;
-//в рублях
+
 int Kop = 4567 * 100;
 Console.WriteLine("В копейках = " + Kop);
+
 int Sotni = 4567 / 100;
 Console.WriteLine("В сотнях = " + Sotni);
+
 int Rubl = K;
 Console.WriteLine("В рублях = " + Rubl);
